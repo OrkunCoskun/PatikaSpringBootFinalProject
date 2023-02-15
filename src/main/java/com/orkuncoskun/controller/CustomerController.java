bfbf13b3a7565b5459ff8cbc337e3ef71ca27887
Orkun Coskun <coskunorkun1@gmail.com>
2023-02-15 14:02:01 +0300

    Views added for frontend: add-customers.html, credit-result.html, list-customers.html, update-customers.html
    CustomerController updated for the views
    CreditApplicationController changed into CustomerRestController

--- src/main/java/com/orkuncoskun/controller/CustomerController.java
+++ src/main/java/com/orkuncoskun/controller/CustomerController.java
@@ -2,68 +2,136 @@ package com.orkuncoskun.controller;
 
 import com.orkuncoskun.business.dto.CustomerDto;
 import com.orkuncoskun.business.services.CustomerServices;
+import com.orkuncoskun.data.entity.CustomerEntity;
+import com.orkuncoskun.data.repository.CustomerRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
-@RestController
-@RequestMapping("/api/v1")
+@Controller
+@RequestMapping({ "/api/v1", "/" })
 @CrossOrigin(origins = "http://localhost:3000")
 public class CustomerController {
-    @Autowired
-    CustomerServices employoeeServices;
+	@Autowired
+	CustomerServices customerServices;
 
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	@GetMapping("/apply")
+	public ModelAndView apply(@RequestParam("customerId") @PathVariable Long customerId, Model model) {
+	    model.addAttribute("customerId", customerId);
+	    ModelAndView mv = new ModelAndView("credit-result");
+		CustomerEntity customer = customerRepository.findById(customerId).orElse(null);
 
-    //ROOT
-    //http://localhost:8080/api/v1/index
-    @GetMapping({"/index","/"}  )
-    public String getRoot(){
-        return "index";
-    }
+		if (customer == null) {
+			mv.addObject("status", "NOT_FOUND");
+			mv.addObject("message", "Customer not found with id: " + customerId);
+			return mv;
+		}
 
-    //LIST
-    //http://localhost:8080/api/v1/customers
-    @GetMapping("/customers")
-    public List<CustomerDto> getAllCustomers(){
-        List<CustomerDto> customerDtoList=employoeeServices.getAllCustomers();
-        return customerDtoList;
-    }
+		double creditScore = customer.getCreditScore();
+		double monthlyIncome = customer.getMonthlyIncome();
+		int creditLimitMultiplier = 4;
 
-    //FIND
-    //http://localhost:8080/api/v1/customers/1
-    @GetMapping("/customers/{id}")
-    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable Long id) throws Throwable {
-        ResponseEntity<CustomerDto> customerDto=  employoeeServices.getCustomerById(id);
-        return customerDto;
-    }
+		mv.addObject("customerId", customerId);
+		mv.addObject("phoneNumber", customer.getPhoneNumber());
 
-    //SAVE
-    //http://localhost:8080/api/v1/customers
-    @PostMapping("/customers")
-    public CustomerDto createCustomer(@RequestBody CustomerDto customerDto){
-        employoeeServices.createCustomers(customerDto);
-        return customerDto;
-    }
+		if (creditScore < 500) {
+			mv.addObject("status", "REJECTED");
+			mv.addObject("message", "Credit score is below 500, credit application rejected.");
+			return mv;
+		} else if (creditScore >= 500 && creditScore < 1000) {
+			if (monthlyIncome < 5000) {
+				mv.addObject("status", "APPROVED");
+				mv.addObject("message", "Credit application approved and assigned limit of 10000 TL.");
+				mv.addObject("limit", 10000);
+				return mv;
+			} else if (monthlyIncome >= 5000 && monthlyIncome < 10000) {
+				mv.addObject("status", "APPROVED");
+				mv.addObject("message", "Credit application approved and assigned limit of 20000 TL.");
+				mv.addObject("limit", 20000);
+				return mv;
+			} else if (monthlyIncome >= 10000) {
+				double limit = monthlyIncome * creditLimitMultiplier / 2;
+				mv.addObject("status", "APPROVED");
+				mv.addObject("message", "Credit application approved and assigned limit of " + limit + " TL.");
+				mv.addObject("limit", limit);
+				return mv;
+			}
+		} else if (creditScore >= 1000) {
+			double limit = monthlyIncome * creditLimitMultiplier;
+			mv.addObject("status", "APPROVED");
+			mv.addObject("message", "Credit application approved and assigned limit of " + limit + " TL.");
+			mv.addObject("limit", limit);
+			return mv;
+		}
 
-    //UPDATE
-    //http://localhost:8080/api/v1/customers/1
-    @PutMapping("/customers/{id}")
-    public ResponseEntity<CustomerDto>  updateCustomer(@PathVariable Long id,  @RequestBody CustomerDto customerDto) throws Throwable {
-        employoeeServices.updateCustomer(id,customerDto);
-        return ResponseEntity.ok(customerDto);
-    }
+		mv.addObject("status", "INTERNAL_ERROR");
+		mv.addObject("message", "Something went wrong.");
+		return mv;
+	}
 
-    //DELETE
-    //http://localhost:8080/api/v1/customers/1
-    @DeleteMapping("/customers/{id}")
-    public ResponseEntity<Map<String,Boolean>> deleteCustomer(@PathVariable Long id) throws Throwable {
-        employoeeServices.deleteCustomer(id);
-        Map<String,Boolean> response=new HashMap<>();
-        response.put("deleted",Boolean.TRUE);
-        return ResponseEntity.ok(response);
-    }
+	// http://localhost:8080/api/v1/viewCustomers
+	@GetMapping({ "/viewCustomers", "/", "/list" })
+	public ModelAndView showCustomersView(Model model) {
+		ModelAndView mv = new ModelAndView("list-customers");
+		List<CustomerEntity> customers = customerRepository.findAll();
+		mv.addObject("customers", customers);
+		return mv;
+	}
+
+	@GetMapping("/addCustomerForm")
+	public ModelAndView addnewCustomersView() {
+		ModelAndView mv = new ModelAndView("add-customers");
+		CustomerEntity newCustomer = new CustomerEntity();
+		mv.addObject("customer", newCustomer);
+		return mv;
+	}
+
+	@PostMapping(value = "/saveCustomer")
+	public ModelAndView saveCustomerView(@ModelAttribute CustomerEntity customer) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("list-customers");
+		customerRepository.save(customer);
+		mv.addObject("customers", customerServices.getAllCustomers());
+
+		return mv;
+	}
+
+	@GetMapping(value = "/updateCustomer")
+	public ModelAndView updateCustomer(@RequestParam long customerId) {
+		ModelAndView mv = new ModelAndView("update-customers");
+		Optional<CustomerEntity> customer = customerRepository.findById(customerId);
+		mv.addObject("customer", customer);
+		return mv;
+	}
+
+	// !!!!!!!!!!!!!!!!!!!There is an error it does not update the customer
+	@PostMapping(value = "updateNewCustomer")
+	public ModelAndView updateNewCustomer(@ModelAttribute CustomerEntity customer) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("customer", customer);
+		mv.setViewName("list-customers");
+		customerRepository.save(customer);
+		mv.addObject("customers", customerServices.getAllCustomers());
+		return mv;
+	}
+
+	@GetMapping(value = "/deleteCustomer")
+	public ModelAndView deleteCustomerView(@RequestParam long customerId) throws Throwable {
+		ModelAndView mv = new ModelAndView("list-customers");
+		customerRepository.deleteById(customerId);
+		mv.addObject("customers", customerServices.getAllCustomers());
+		return mv;
+	}
 }
--- src/main/java/com/orkuncoskun/controller/CustomerRestController.java
+++ src/main/java/com/orkuncoskun/controller/CustomerRestController.java
@@ -1,23 +1,34 @@
 package com.orkuncoskun.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.orkuncoskun.business.dto.CustomerDto;
+import com.orkuncoskun.business.services.CustomerServices;
 import com.orkuncoskun.data.entity.CustomerEntity;
 import com.orkuncoskun.data.repository.CustomerRepository;
 
 @RestController
-@RequestMapping("/api/v1")
-public class CreditApplicationController {
+@RequestMapping({ "/api/v1", "/" })
+@CrossOrigin(origins = "http://localhost:3000")
+public class CustomerRestController {
+
+	@Autowired
+	CustomerServices customerServices;
 
 	@Autowired
 	private CustomerRepository customerRepository;
@@ -73,4 +84,47 @@ public class CreditApplicationController {
 	    response.put("message", "Something went wrong.");
 	    return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+
+		// LIST
+		// http://localhost:8080/api/v1/customers
+		@GetMapping("/customers")
+		public List<CustomerDto> getAllCustomers() {
+			List<CustomerDto> customerDtoList = customerServices.getAllCustomers();
+			return customerDtoList;
+		}
+
+		// FIND
+		// http://localhost:8080/api/v1/customers/1
+		@GetMapping("/customers/{id}")
+		public ResponseEntity<CustomerDto> getCustomerById(@PathVariable Long id) throws Throwable {
+			ResponseEntity<CustomerDto> customerDto = customerServices.getCustomerById(id);
+			return customerDto;
+		}
+
+		// SAVE
+		// http://localhost:8080/api/v1/customers
+		@PostMapping("/customers")
+		public CustomerDto createCustomer(@RequestBody CustomerDto customerDto) {
+			customerServices.createCustomers(customerDto);
+			return customerDto;
+		}
+
+		// UPDATE
+		// http://localhost:8080/api/v1/customers/1
+		@PutMapping("/customers/{id}")
+		public ResponseEntity<CustomerDto> updateCustomer(@PathVariable Long id, @RequestBody CustomerDto customerDto)
+				throws Throwable {
+			customerServices.updateCustomer(id, customerDto);
+			return ResponseEntity.ok(customerDto);
+		}
+
+		// DELETE
+		// http://localhost:8080/api/v1/customers/1
+		@DeleteMapping("/customers/{id}")
+		public ResponseEntity<Map<String, Boolean>> deleteCustomer(@PathVariable Long id) throws Throwable {
+			customerServices.deleteCustomer(id);
+			Map<String, Boolean> response = new HashMap<>();
+			response.put("deleted", Boolean.TRUE);
+			return ResponseEntity.ok(response);
+		}
 }
